7f2a4cc1de259fa6867ac7829090a02bfb835c0e
david050708 <85152515+david050708@users.noreply.github.com>
2021-06-09 12:19:51 +0530

    Add files via upload

---
+++ InstantHear.py
@@ -0,0 +1,22 @@
+import pyttsx3
+from tkinter import *
+import tkinter as tk
+
+root = tk.Tk()
+root.geometry("500x500")
+root.title("Instant Hear")
+root.iconbitmap("C:/Users/kani/Downloads/hnet.com-image.ico")
+file_path = ""
+
+
+text=Entry(root,width=50,bg="white",fg="black",font=("arial","16"))
+text.pack(padx=10,pady=20)
+
+def Hear():
+    pyttsx3.speak(text.get())
+    
+
+x = tk.StringVar()
+b1 = tk.Button(root,text="Hear",fg="black",bg="lightgrey",command=Hear).pack()
+
+root.mainloop()
